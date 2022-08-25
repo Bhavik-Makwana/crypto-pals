e9ab8d77615a515e922619dfa1bd29906f28a73d
Bhav <bhavikmakwana09@gmail.com>
2022-08-25 12:52:58 +0100

    cbc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 #![feature(test)]
+extern crate base64;
+extern crate hex;
 extern crate test;
 mod errors;
 mod set1;
+mod set2;
 
 fn main() {
-    println!("msg: {:?}", set1::detect_ecb("input/set1_challenge8.txt"));
+    let ciphertext = hex::encode(
+        base64::decode(set1::io::read_file_no_newline("input/set2_challenge10.txt")).unwrap(),
+    );
+    let key = "YELLOW SUBMARINE";
+    let iv: String = (0..16).map(|_| 0 as u8 as char).collect();
+    // let ciphertext = set2::aes128_cbc_encrypt(plaintext, key, &iv);
+    let decrypted_text = set2::aes128_cbc_decrypt(&ciphertext, key, &iv);
+    println!("msg: {:?}", decrypted_text);
 }
